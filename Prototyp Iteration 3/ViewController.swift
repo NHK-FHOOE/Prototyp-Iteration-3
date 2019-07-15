5bf8583178966c8ed47bcf3553f98aceac920861
Niels-Hendrik Klopsch <niels-hendrikklopsch@nielshedriksmbp.local>
2019-07-15 19:15:45 +0200

    Initial Commit

---
+++ Prototyp Iteration 3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototyp Iteration 3
+//
+//  Created by Niels-Hendrik Klopsch on 15.07.19.
+//  Copyright © 2019 Niels-Hendrik Klopsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
